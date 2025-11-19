5624a1bfd64aa999a46d7820ed79f676bfe20d92
annavanong <anna.vanongeval@student.howest.be>
2025-11-19 11:57:02 +0100

    update site URL in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://your-github-username.github.io',
+  site: 'https://annavanong2003.github.io',
   base: '/cck/',
 });
